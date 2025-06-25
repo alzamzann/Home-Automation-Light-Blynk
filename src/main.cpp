262ab02261c19c45b4289d70c96ff28a8ac3191b
M. Fauzan Al Zamzami <alzamzann@gmail.com>
2025-06-25 21:13:29 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,251 @@
+/* Fill-in your Template ID Device Name and Auth Token */
+#define BLYNK_TEMPLATE_ID "TMPL6F209WQyX"
+#define BLYNK_TEMPLATE_NAME "Projek Sistem Tertanam"
+#define BLYNK_AUTH_TOKEN "ihE6aELBWx7UcKC5fH-aKwyrV3GW4hrC"
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Zamspot";
+char pass[] = "1sampaii9";
+
+
+//#define BLYNK_PRINT Serial
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include <Preferences.h>
+
+Preferences pref;
+
+// define the GPIO connected with Relays and switches
+#define RelayPin1 23  //D23
+#define RelayPin2 22  //D22
+#define RelayPin3 21  //D21
+#define RelayPin4 19  //D19
+
+#define SwitchPin1 13  //D13
+#define SwitchPin2 12  //D12
+#define SwitchPin3 14  //D14
+#define SwitchPin4 27  //D27
+
+#define wifiLed   2   //D2
+
+//Change the virtual pins according yours
+#define VPIN_BUTTON_1    V1
+#define VPIN_BUTTON_2    V2
+#define VPIN_BUTTON_3    V3
+#define VPIN_BUTTON_4    V4
+#define VPIN_BUTTON_C    V5
+
+
+
+
+// Relay State
+bool toggleState_1 = LOW; //Define integer to remember the toggle state for relay 1
+bool toggleState_2 = LOW; //Define integer to remember the toggle state for relay 2
+bool toggleState_3 = LOW; //Define integer to remember the toggle state for relay 3
+bool toggleState_4 = LOW; //Define integer to remember the toggle state for relay 4
+
+// Switch State
+bool SwitchState_1 = LOW;
+bool SwitchState_2 = LOW;
+bool SwitchState_3 = LOW;
+bool SwitchState_4 = LOW;
+
+int wifiFlag = 0;
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+BlynkTimer timer;
+
+void checkBlynkStatus() { // called every 3 seconds by SimpleTimer
+
+  bool isconnected = Blynk.connected();
+  if (isconnected == false) {
+    wifiFlag = 1;
+    digitalWrite(wifiLed, LOW);
+    Serial.println("Blynk Not Connected");
+  }
+  if (isconnected == true) {
+    wifiFlag = 0;
+    digitalWrite(wifiLed, HIGH);
+    Serial.println("Blynk Connected");
+  }
+}
+
+void manual_control()
+{
+  if (digitalRead(SwitchPin1) == LOW && SwitchState_1 == LOW) {
+    digitalWrite(RelayPin1, LOW);
+    toggleState_1 = HIGH;
+    SwitchState_1 = HIGH;
+    pref.putBool("Relay1", toggleState_1);
+    Blynk.virtualWrite(VPIN_BUTTON_1, toggleState_1);
+    Serial.println("Switch-1 on");
+  }
+  if (digitalRead(SwitchPin1) == HIGH && SwitchState_1 == HIGH) {
+    digitalWrite(RelayPin1, HIGH);
+    toggleState_1 = LOW;
+    SwitchState_1 = LOW;
+    pref.putBool("Relay1", toggleState_1);
+    Blynk.virtualWrite(VPIN_BUTTON_1, toggleState_1);
+    Serial.println("Switch-1 off");
+  }
+  if (digitalRead(SwitchPin2) == LOW && SwitchState_2 == LOW) {
+    digitalWrite(RelayPin2, LOW);
+    toggleState_2 = HIGH;
+    SwitchState_2 = HIGH;
+    pref.putBool("Relay2", toggleState_2);
+    Blynk.virtualWrite(VPIN_BUTTON_2, toggleState_2);
+    Serial.println("Switch-2 on");
+  }
+  if (digitalRead(SwitchPin2) == HIGH && SwitchState_2 == HIGH) {
+    digitalWrite(RelayPin2, HIGH);
+    toggleState_2 = LOW;
+    SwitchState_2 = LOW;
+    pref.putBool("Relay2", toggleState_2);
+    Blynk.virtualWrite(VPIN_BUTTON_2, toggleState_2);
+    Serial.println("Switch-2 off");
+  }
+  if (digitalRead(SwitchPin3) == LOW && SwitchState_3 == LOW) {
+    digitalWrite(RelayPin3, LOW);
+    toggleState_3 = HIGH;
+    SwitchState_3 = HIGH;
+    pref.putBool("Relay3", toggleState_3);
+    Blynk.virtualWrite(VPIN_BUTTON_3, toggleState_3);
+    Serial.println("Switch-3 on");
+  }
+  if (digitalRead(SwitchPin3) == HIGH && SwitchState_3 == HIGH) {
+    digitalWrite(RelayPin3, HIGH);
+    toggleState_3 = LOW;
+    SwitchState_3 = LOW;
+    pref.putBool("Relay3", toggleState_3);
+    Blynk.virtualWrite(VPIN_BUTTON_3, toggleState_3);
+    Serial.println("Switch-3 off");
+  }
+  if (digitalRead(SwitchPin4) == LOW && SwitchState_4 == LOW) {
+    digitalWrite(RelayPin4, LOW);
+    toggleState_4 = HIGH;
+    SwitchState_4 = HIGH;
+    pref.putBool("Relay4", toggleState_4);
+    Blynk.virtualWrite(VPIN_BUTTON_4, toggleState_4);
+    Serial.println("Switch-4 on");
+  }
+  if (digitalRead(SwitchPin4) == HIGH && SwitchState_4 == HIGH) {
+    digitalWrite(RelayPin4, HIGH);
+    toggleState_4 = LOW;
+    SwitchState_4 = LOW;
+    pref.putBool("Relay4", toggleState_4);
+    Blynk.virtualWrite(VPIN_BUTTON_4, toggleState_4);
+    Serial.println("Switch-4 off");
+  }
+}
+
+void all_SwitchOff() {
+  toggleState_1 = 0; digitalWrite(RelayPin1, HIGH); pref.putBool("Relay1", toggleState_1); Blynk.virtualWrite(VPIN_BUTTON_1, toggleState_1); delay(100);
+  toggleState_2 = 0; digitalWrite(RelayPin2, HIGH); pref.putBool("Relay2", toggleState_2); Blynk.virtualWrite(VPIN_BUTTON_2, toggleState_2); delay(100);
+  toggleState_3 = 0; digitalWrite(RelayPin3, HIGH); pref.putBool("Relay3", toggleState_3); Blynk.virtualWrite(VPIN_BUTTON_3, toggleState_3); delay(100);
+  toggleState_4 = 0; digitalWrite(RelayPin4, HIGH); pref.putBool("Relay4", toggleState_4); Blynk.virtualWrite(VPIN_BUTTON_4, toggleState_4); delay(100);
+}
+
+void getRelayState()
+{
+  //Serial.println("reading data from NVS");
+  toggleState_1 = pref.getBool("Relay1", 0);
+  digitalWrite(RelayPin1, !toggleState_1);
+  Blynk.virtualWrite(VPIN_BUTTON_1, toggleState_1);
+  delay(200);
+  toggleState_2 = pref.getBool("Relay2", 0);
+  digitalWrite(RelayPin2, !toggleState_2);
+  Blynk.virtualWrite(VPIN_BUTTON_2, toggleState_2);
+  delay(200);
+  toggleState_3 = pref.getBool("Relay3", 0);
+  digitalWrite(RelayPin3, !toggleState_3);
+  Blynk.virtualWrite(VPIN_BUTTON_3, toggleState_3);
+  delay(200);
+  toggleState_4 = pref.getBool("Relay4", 0);
+  digitalWrite(RelayPin4, !toggleState_4);
+  Blynk.virtualWrite(VPIN_BUTTON_4, toggleState_4);
+  delay(200);
+}
+
+BLYNK_CONNECTED() {
+  // update the latest state to the server
+  Blynk.virtualWrite(VPIN_BUTTON_1, toggleState_1);
+  Blynk.virtualWrite(VPIN_BUTTON_2, toggleState_2);
+  Blynk.virtualWrite(VPIN_BUTTON_3, toggleState_3);
+  Blynk.virtualWrite(VPIN_BUTTON_4, toggleState_4);
+}
+
+// When App button is pushed - switch the state
+
+BLYNK_WRITE(VPIN_BUTTON_1) {
+  toggleState_1 = param.asInt();
+  digitalWrite(RelayPin1, !toggleState_1);
+  pref.putBool("Relay1", toggleState_1);
+}
+
+BLYNK_WRITE(VPIN_BUTTON_2) {
+  toggleState_2 = param.asInt();
+  digitalWrite(RelayPin2, !toggleState_2);
+  pref.putBool("Relay2", toggleState_2);
+}
+
+BLYNK_WRITE(VPIN_BUTTON_3) {
+  toggleState_3 = param.asInt();
+  digitalWrite(RelayPin3, !toggleState_3);
+  pref.putBool("Relay3", toggleState_3);
+}
+
+BLYNK_WRITE(VPIN_BUTTON_4) {
+  toggleState_4 = param.asInt();
+  digitalWrite(RelayPin4, !toggleState_4);
+  pref.putBool("Relay4", toggleState_4);
+}
+
+BLYNK_WRITE(VPIN_BUTTON_C) {
+  all_SwitchOff();
+}
+
+void setup()
+{
+  Serial.begin(115200);;
+  //Open namespace in read-write mode
+  pref.begin("Relay_State", false);
+
+  pinMode(RelayPin1, OUTPUT);
+  pinMode(RelayPin2, OUTPUT);
+  pinMode(RelayPin3, OUTPUT);
+  pinMode(RelayPin4, OUTPUT);
+
+  pinMode(wifiLed, OUTPUT);
+
+  pinMode(SwitchPin1, INPUT_PULLUP);
+  pinMode(SwitchPin2, INPUT_PULLUP);
+  pinMode(SwitchPin3, INPUT_PULLUP);
+  pinMode(SwitchPin4, INPUT_PULLUP);
+
+  //During Starting all Relays should TURN OFF
+  digitalWrite(RelayPin1, !toggleState_1);
+  digitalWrite(RelayPin2, !toggleState_2);
+  digitalWrite(RelayPin3, !toggleState_3);
+  digitalWrite(RelayPin4, !toggleState_4);
+
+  digitalWrite(wifiLed, LOW);
+
+  WiFi.begin(ssid, pass);
+  timer.setInterval(2000L, checkBlynkStatus); // check if Blynk server is connected every 2 seconds
+  Blynk.config(auth);
+  delay(1000);
+
+  getRelayState(); //fetch data from NVS Flash Memory
+  //  delay(1000);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates SimpleTimer
+
+  manual_control();
+}
